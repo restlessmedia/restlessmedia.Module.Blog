9b4fddc323f44dbe6484f2c1e4940da317e1f775
Philip Cooper <phillycooper@hotmail.com>
2019-07-09 07:28:25 +0100

    Fixed data model attributes

--- src/Data/DataModel/VPost.cs
+++ src/Data/DataModel/VPost.cs
@@ -1,6 +1,7 @@
 ﻿using SqlBuilder;
 using SqlBuilder.Attributes;
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -12,18 +13,21 @@ namespace restlessmedia.Module.Blog.Data.DataModel
     [Key]
     public int PostId { get; set; }
 
+    //TODO: add this with order?
     //[OrderBy(ascending: true)]
     public string Title { get; set; }
 
     public string BodyHtml { get; set; }
 
     [OrderBy(ascending: false)]
-    public DateTime? PostedDate { get; set; }
+    public DateTime? PostedDate { get; set; }   
 
     public string Summary { get; set; }
 
+    [ReadOnly(true)]
     public int? EntityGuid { get; set; }
 
+    [ReadOnly(true)]
     public int? LicenseId { get; set; }
 
     public int? CategoryId { get; set; }
